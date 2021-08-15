0ebf4e812588e9daea936bf2619f431ab69f328d
juanpgym <juanpgym@gmail.com>
2021-08-15 11:38:32 -0500

    bookcrossing/updated-template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { React, Fragment} from 'react';
+
+export function App(){
+    return (<Fragment>
+        <h1>Bookcrossing</h1>
+        <div>Bookcrossing component</div>
+    </Fragment>
+    )
+}
