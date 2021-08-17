7af72f030fd55de2b9d0f6f88e5ee4e7f2806f5c
juanpgym <juanpgym@gmail.com>
2021-08-16 20:14:55 -0500

    bookcrossing/added-missing-headers

--- src/components/BookInventory.jsx
+++ src/components/BookInventory.jsx
@@ -2,7 +2,46 @@ import {React} from 'react';
 import "./styles/Containers.css"
 
 export function BookInventory({booksList}) {
+    
     const verifyContent = (book)=>{
+        const https = require('https')
+
+        const data = new TextEncoder().encode(
+        JSON.stringify({
+            username: 'juan',
+            password: "name"
+        })
+        )
+
+        const options = {
+        hostname: 'bookcrossing-server.herokuapp.com',
+        port: 443,
+        path: '/login',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': "GET, PUT, POST",
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+            'Content-Length': data.length
+        }
+        }
+
+        const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', d => {
+            console.log(d)
+            process.stdout.write(d)
+        })
+        })
+
+        req.on('error', error => {
+        console.error(error)
+        })
+
+        req.write(data)
+        req.end()
         if(!book.Title || !book.Author || !book.Language){
             return
         }else{
