6ee79cb03a950328d413e95a6f4eb6d97e5ba805
juanpgym <juanpgym@gmail.com>
2021-08-18 08:18:50 -0500

    bookcrossing/added-signup-request

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,40 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 
 export function SignUp() {
+    let history = useHistory();
+    const signupRequest = () => {
+        const usr = JSON.stringify(document.getElementById('userField').value);
+        const pass = JSON.stringify(document.getElementById('passwordField').value);
+        const email = JSON.stringify(document.getElementById('mailField').value);
+        const cel = JSON.stringify(document.getElementById('celField').value);
+        var axios = require('axios');
+        var data = JSON.stringify({
+        "username": usr,
+        "password": pass,
+        "email": email,
+        "cel": cel
+        });
+        console.log(process.env.NODE_ENV);
+        var baseUrl = process.env.NODE_ENV==='development'? 'http://localhost:4000/signup':'https://bookcrossing-server.herokuapp.com/signup' ;
+        console.log(baseUrl);
+        var config = {
+        method: 'post',
+        url: baseUrl,
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        data : data
+        };
+        axios(config)
+        .then(function (response) {
+            response? history.push("/login"):console.log("signup error!")
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+    }
+
     return (
         <div className = "login_container">
             <h2>Sign Up</h2>
@@ -8,7 +42,7 @@ export function SignUp() {
             <input id="passwordField" type="text" placeholder="Contraseña"></input>
             <input id="mailField" type="text" placeholder="Correo"></input>
             <input id="celField" type="text" placeholder="Cel."></input>
-            <button >Sign Up</button>
+            <button onClick={signupRequest}>Sign Up</button>
         </div>
     )
 }
