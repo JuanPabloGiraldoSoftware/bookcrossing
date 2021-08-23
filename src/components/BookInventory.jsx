e940fc374a9f9cec75bbed225c60060f2ffe5c8e
juanpgym <juanpgym@gmail.com>
2021-08-23 11:36:18 -0500

    bookcrossing/fixed-book-back-ground-size

--- src/components/BookInventory.jsx
+++ src/components/BookInventory.jsx
@@ -1,25 +1,27 @@
 import {React} from 'react';
 import "./styles/Containers.css"
 import "./styles/Buttons.css"
+import {getCurrentUsr} from '../App';
 
 export function BookInventory({booksList}) {
     
     const verifyContent = (book)=>{
-        if(!book.title || !book.author || !book.language){
+        if(!book.title || !book.author || !book.language || !book.gender || !book.year){
             return
         }else{
+            const owner = book.owner?book.owner:book.userName;
            return(
                <div className="col-2 singlebook_container">
-                <h3 style={{color:"yellow"}}>{book.title}</h3>
+                <h6 style={{color:"yellow"}}>{book.title}</h6>
                 <ul>
                     <li><b>Autor:</b> {book.author}</li>
                     <li><b>Idioma:</b> {book.language}</li>
                     <li><b>Género:</b> {book.gender}</li>
                     <li><b>Año:</b> {book.year}</li>
-                    <li><b>Usuario:</b> {book.owner?book.owner:book.userName}</li>
+                    <li><b>Usuario:</b> {owner}</li>
                     
                 </ul>
-                <div className="select_button"><button>Me gusta</button></div>
+                {getCurrentUsr()!== owner?<div className="select_button"><button>Me gusta</button></div>:null}
                 </div>)
         }
     }
