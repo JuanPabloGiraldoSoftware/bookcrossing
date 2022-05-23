11c13cd175e5f01651f9649ff5105ed5be64eea8
JuanPabloGiraldoSoftware <juanpgym@gmail.com>
2022-05-23 01:17:39 -0500

    added-automatic-test

--- automatic_testing/robot.py
+++ automatic_testing/robot.py
@@ -1,6 +1,5 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -8,7 +7,6 @@ driver = Chrome("./chromedriver.exe")
 driver.get("https://bookcrossingpuj.netlify.app/")
 
 print(driver.current_url)
-
 
 def automatic_login(usr,pwd):
     element_pointer = driver.find_element(by=By.ID, value="loginButton")
@@ -39,22 +37,54 @@ def automatic_test_signup(usr,pwd,email,cel):
     element_pointer.click()
 
 def automatic_test_books_addition(title, author, language, gender, year):
-    automatic_login("natalia1","natalia1*")
     element_pointer = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "addingS")))
     element_pointer.click()
     element_pointer = driver.find_element(by=By.ID, value="bookTitleField")
+    element_pointer.clear()
+    if type(title) is not str:
+        return False
     element_pointer.send_keys(title)
     element_pointer = driver.find_element(by=By.ID, value="bookAuthorField")
+    element_pointer.clear()
+    if type(author) is not str:
+        return False
     element_pointer.send_keys(author)
     element_pointer = driver.find_element(by=By.ID, value="bookLanguage")
+    element_pointer.clear()
+    if type(language) is not str:
+        return False
     element_pointer.send_keys(language)
     element_pointer = driver.find_element(by=By.ID, value="bookGender")
+    element_pointer.clear()
+    if type(gender) is not str:
+        return False
     element_pointer.send_keys(gender)
     element_pointer = driver.find_element(by=By.ID, value="bookYear")
+    element_pointer.clear()
+    if type(year) is not str:
+        return False
     element_pointer.send_keys(year)
-    element_pointer = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "addBookB")))
+    element_pointer = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "addBookB")))
     element_pointer.click()
+    return True
 
-automatic_test_books_addition("automaticBook1","autoAuthor1","autoLeng1","autoGender1","1984")
-close_session()
-automatic_test_signup("autoUser", "autoPass", "auto@javerianacali.edu.co", "1234567891")
+automatic_login("natalia1","natalia1*")
+input_data = open('input_auto_test.txt')
+i=1
+passed=0
+failed=0
+for line in input_data:
+    data=line.split(',')
+    result=automatic_test_books_addition(data[0],data[1],data[2],data[3],data[4])
+    if result:
+        print("Test #"+str(i)+" passed!")
+        passed+=1
+    else:
+        print("Test #"+str(i)+" failed!")
+        failed+=1
+    i+=1
+
+print("Test passed: "+str(passed))
+print("Test failed: "+str(failed))
+
+#driver.quit()
